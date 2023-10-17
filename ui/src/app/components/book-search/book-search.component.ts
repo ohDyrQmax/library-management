997a92687d0ccbb9dd3270492371e75f6cf5691a
ohDyrQmax <ohdyr0699@gmail.com>
2023-10-17 14:11:12 +0700

    update search book with filter component

--- ui/src/app/components/book-search/book-search.component.ts
+++ ui/src/app/components/book-search/book-search.component.ts
@@ -1,46 +1,45 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
+import {Router} from '@angular/router';
 import {Category} from "../../common/category.component";
 import {Author} from "../../common/author.component";
 
 @Component({
-  selector: 'app-book-search',
-  templateUrl: './book-search.component.html',
-  styleUrls: ['./book-search.component.css']
+    selector: 'app-book-search',
+    templateUrl: './book-search.component.html',
+    styleUrls: ['./book-search.component.css']
 })
 export class BookSearchComponent implements OnInit{
 
-  constructor(
-    private router: Router,
-    private activatedRoute: ActivatedRoute
-  ) {
-    let title = this.activatedRoute.snapshot.queryParams['title'];
-    let author = this.activatedRoute.snapshot.queryParams['author'];
-    let category = this.activatedRoute.snapshot.queryParams['category'];
-    sessionStorage.setItem('searchKeyword', title === "undefined" ? "" : title);
-    sessionStorage.setItem('selectedAuthor', author === "undefined" ? "" : author);
-    sessionStorage.setItem('selectedCategory', category === "undefined" ? "" : category);
-  }
+    constructor(
+        private router: Router,
+    ) {
+        // let title = this.activatedRoute.snapshot.queryParams['title'];
+        // let author = this.activatedRoute.snapshot.queryParams['author'];
+        // let category = this.activatedRoute.snapshot.queryParams['category'];
+        // sessionStorage.setItem('searchKeyword', title === "undefined" ? "" : title);
+        // sessionStorage.setItem('selectedAuthor', author === "undefined" ? "" : author);
+        // sessionStorage.setItem('selectedCategory', category === "undefined" ? "" : category);
+    }
 
-  ngOnInit() {
+    ngOnInit() {
 
-  }
+    }
 
-  @Input() bookAuthors!: { [key: string]: Author };
-  @Input() bookCategories!: { [key: string]: Category };
+    @Input() bookAuthors!: { [key: string]: Author };
+    @Input() bookCategories!: { [key: string]: Category };
 
-  searchKeyword: string = sessionStorage.getItem('searchKeyword');
-  selectedAuthor: string = sessionStorage.getItem('selectedAuthor');
-  selectedCategory: string = sessionStorage.getItem('selectedCategory');
+    searchKeyword: string = sessionStorage.getItem('searchKeyword');
+    selectedAuthor: string = sessionStorage.getItem('selectedAuthor');
+    selectedCategory: string = sessionStorage.getItem('selectedCategory');
 
-  changeFilterValue(e: any) {
-    this[e.target.name] = e.target.value;
-    sessionStorage.setItem(e.target.name, e.target.value);
-  }
+    changeFilterValue(e: any) {
+        this[e.target.name] = e.target.value;
+        sessionStorage.setItem(e.target.name, e.target.value);
+    }
 
-  searchBooks() {
-    this.router.navigateByUrl(`books?title=${this.searchKeyword}&author=${this.selectedAuthor}&category=${this.selectedCategory}`);
-  }
+    searchBooks() {
+        this.router.navigateByUrl(`books?title=${this.searchKeyword}&author=${this.selectedAuthor}&category=${this.selectedCategory}`);
+    }
 
-  protected readonly sessionStorage = sessionStorage;
+    protected readonly sessionStorage = sessionStorage;
 }
